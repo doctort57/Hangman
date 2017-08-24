b0b3dbd2d619cb3d6a46f097a7d3028c0be3a765
David Thurman <dvdthurman@gmail.com>
2017-08-24 17:19:23 -0400

    08/22/2017 Final

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -8,20 +8,19 @@ var games_total = 0;
 
 // object Array for selected words and Winning message, image, sounds
 var winnerObject = {
-    words: ["BRUCE","ROY","CLARENCE","NILS","MAX","STEVIE"],
-    images: ["BRUCE2.jpg","Roy2.jpg","Clarence.jpg","NILS3.jpeg","max.jpg","Stevie.jpg"],
-    winMsg: ["Lucky Break, try again","Cheater","Wise Guy","I dont like playing with You",
-						"Okay you are pretty good at this arent You"],
-    winSound: ["needed_that.wav","foot_down.wav","is_this_great.wav","lyin_around.wav","pledges.wav",
-						   "toga.wav","down_the_drain.wav"]
+    words: ["BLUTO","FLOUNDER","DEANWORMER","DDAY"],
+    images: ["TOGA.jpg","Flounder.jpg","doubleSecret.jpg","rammingSpeed.jpg"],
+    winMsg: ["TOGA TOGA TOGA TOGA","Oh Man is this Great","Double Secret Probation",
+						"Ramming Speed!!!"],
+    winSound: ["toga.wav","is_this_great.wav","double_secret.wav","ramming.wav"]
  }
 
 // object Array for Losing message, image, sounds
-var LoserObject = {
-	imageWrg: ["HeyWhats.jpg","Wormer.jpg","Pledgepin.jpg","Bluto.jpg","DDay.jpg","TOGA.jpg"],
-    wrgMsg: ["Bluto","Wormer DEAD MAN","Hey Needermyer","Drink Heavily","DDAY","TOGA!! TOGA!! TOGA!!"],
-    playSound: ["needed_that.wav","foot_down.wav","is_this_great.wav","lyin_around.wav","pledges.wav",
-						   "toga.wav","down_the_drain.wav"]
+var loserObject = {
+	imageWrg: ["bluto.jpg","Smartguy.jpg","LyingAround.jpg","Pledgepin.jpg"],
+    wrgMsg: ["My Advice to You is to Start Drinking Heavily","You'll get your chance Smart Guy",
+    		 "What's all this Lying Around Stuff?", "For God's sake! What kind of man hits a defenseless animal??"],
+    loserSound: ["advice.wav","chance.wav","lyin_around.wav","defenseless.wav"]
 };
 
 /* Main function for eacch clicked letter */
@@ -67,12 +66,11 @@ if (display_word.indexOf("#") == -1)
 		games_won +=1;
 		document.game.gamesWon.value = games_won;
 // set random image message and sound on Winning Game		
-		document.bruce.src="assets/images/" + winnerObject.images[random_number]; 
-		random_number = Math.round(Math.random() * (winnerObject.winMsg.length - 1));
-	/*	var audio = new Audio("assets/sounds/" + winnerObject.playSound[random_number]);
-		audio.play(); */
-		alert(winnerObject.winMsg[random_number]);
-		reset();
+		document.bluto.src="assets/images/" + winnerObject.images[random_number]; 
+		var audio = new Audio("assets/sounds/" + winnerObject.winSound[random_number]);
+		audio.play(); 
+		alert("WINNER " + winnerObject.winMsg[random_number]);
+		this.reset();
 	}
 }
 else
@@ -84,11 +82,11 @@ else
 				games_lost +=1;
 // set random image message and sound on Loss 
 				random_number = Math.round(Math.random() * (loserObject.imageWrg.length - 1));
-				document.bruce.src="assets/images/" + loserObject.imageWrg[random_number];
-				var audio = new Audio("assets/sounds/" + loserObject.playSound[random_number]);
+				document.bluto.src="assets/images/" + loserObject.imageWrg[random_number];
+				var audio = new Audio("assets/sounds/" + loserObject.loserSound[random_number]);
 				audio.play(); 
-   				alert("Loser" + loserObject.wrgMsg[random_number]);
-				reset();
+   				alert("LOSER " + loserObject.wrgMsg[random_number]);
+				this.reset();
 			}
 	}
 }
@@ -97,7 +95,7 @@ else
 /* Reset game after each win or loss */
 
 function reset(){
-		selectWord();
+		this.selectWord();
 		document.game.usedLetters.value = "";
 		used_letters = "";
 		wrong_guesses = 0;
@@ -106,7 +104,7 @@ function reset(){
 		document.game.gamesWon.value = games_won;
 		document.game.gamesLost.value = games_lost;
 		if (games_total == 0) {
-			document.bruce.src="assets/images/PEOPLE.jpg"; 
+			document.bluto.src="assets/images/AnimalHouse.jpg"; 
 		} 
 }
 
@@ -117,7 +115,7 @@ function selectWord()
 	random_number = Math.round(Math.random() * winnerObject.words.length - 1);
 	to_guess = winnerObject.words[random_number];
 // display masked word
-	masked_word = createMask(to_guess);
+	masked_word = this.createMask(to_guess);
 	document.game.displayWord.value = masked_word;
 	display_word = masked_word;
 }
@@ -127,9 +125,9 @@ function selectWord()
 function createMask(m)
 {
 	mask = "";
-	word_lenght = m.length;
+	word_length = m.length;
  
-for (i = 0; i < word_lenght; i ++)
+for (i = 0; i < word_length; i ++)
 {
 	mask += "#";
 }
